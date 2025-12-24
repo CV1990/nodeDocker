229a2b1f581e499a19a583ef4efe23d60c163269
CV1990 <carlos.rivas.rm@gmail.com>
2025-12-23 19:35:59 -0600

    add new route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   // Verificamos que el path sea exactamente la raíz "/"
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(JSON.stringify({ mensaje: "Hola Mundo!" }));
+  }
+
   if (req.url === '/status') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(JSON.stringify({ status: 200, mensaje: "Ok" }));
