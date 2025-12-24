d521f0178af635c8e6f689ecee1c758b7108f8af
CV1990 <carlos.rivas.rm@gmail.com>
2025-12-23 18:45:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Verificamos que el path sea exactamente la raíz "/"
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(JSON.stringify({ mensaje: "Hola Mundo Dockerizado!" }));
+  } else {
+    // Para cualquier otra ruta (ej: /usuarios) devolvemos 404
+    res.writeHead(404);
+    res.end('Ruta no encontrada');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000/');
+});
